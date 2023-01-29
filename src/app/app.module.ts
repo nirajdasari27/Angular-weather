9275a5cd023d6c31a6162952d6f7efa03af3e6af
Nayan <nayanyadav219@gmail.com>
2023-01-29 17:38:05 +0530

    component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DetailsComponent } from './details/details.component';
+import { SearchComponent } from './search/search.component';
+import { DisplayComponent } from './display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent,
+    SearchComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
